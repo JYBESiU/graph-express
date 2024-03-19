25074cd81f41c9c568860bcd4b75146534cf6740
JYBESiU <jychoi.besiu@gmail.com>
2024-03-20 01:37:18 +0900

    💬 add colors

--- src/constant.ts
+++ src/constant.ts
@@ -1,1 +1,15 @@
+import { NodeLabel } from "./types";
 export const port = 3001;
+
+export const nodeColors = {
+  [NodeLabel.CITY]: "#8ED3C7",
+  [NodeLabel.COMPANY]: "#CCEBC5",
+  [NodeLabel.CONTINENT]: "#D9D9D9",
+  [NodeLabel.COUNTRY]: "#FFFFB3",
+  [NodeLabel.FORUM]: "#B3DE68",
+  [NodeLabel.MESSAGE]: "#BDBAD9",
+  [NodeLabel.PERSON]: "#FDB462",
+  [NodeLabel.TAG]: "#FCCDE5",
+  [NodeLabel.TAGCLASS]: "#BB80BC",
+  [NodeLabel.UNIVERSITY]: "#FFED6F",
+};
--- src/types.ts
+++ src/types.ts
@@ -11,22 +11,34 @@ export enum NodeLabel {
   UNIVERSITY = "university",
 }
 
-export enum EdgeLabel {}
+/**
+ * city : 300
+ * company : 300
+ * continent x
+ * country : 30
+ * forum 1000
+ * message :  100000
+ * person : 2000
+ * tag 100
+ * tagclass 100
+ * univ 1600
+ */
 
-//     -- city_ispartof_country,
-//     -- country_ispartof_continent,
-//     -- forum_containerof_message,
-//     -- forum_hasmember_person,
-//     -- forum_hasmoderator_person,
-//     -- forum_hastag_tag,
-//     -- message_hascreator_person,
-//     -- message_hastag_tag,
-//     -- message_replyof_message,
-//     -- person_hasinterest_tag,
-//     -- person_islocatedin_city,
-//     -- person_knows_person,
-//     -- person_likes_message,
-//     -- person_studyat_university,
-//     -- person_workat_company,
-//     -- tag_hastype_tc,
-//     -- tc_issubclassof_tc
+export enum EdgeLabel {}
+// -- city_ispartof_country,
+// -- country_ispartof_continent,
+// -- forum_containerof_message,
+// -- forum_hasmember_person,
+// -- forum_hasmoderator_person,
+// -- forum_hastag_tag,
+// -- message_hascreator_person,
+// -- message_hastag_tag,
+// -- message_replyof_message,
+// -- person_hasinterest_tag,
+// -- person_islocatedin_city,
+// -- person_knows_person,
+// -- person_likes_message,
+// -- person_studyat_university,
+// -- person_workat_company,
+// -- tag_hastype_tc,
+// -- tc_issubclassof_tc
